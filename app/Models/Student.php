239662d606db6ba2356a668168adfb1eb9610a75
Your Name <you@example.com>
2023-04-17 23:57:48 +0200

    files

---
+++ app/Models/Student.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+class Student{
+public static function all(){
+$students=[
+    [
+        'id'=> 1 ,
+        'name'=> 'Abdullah'
+    ],
+    [
+        'id'=> 2,
+        'name'=> 'Adel'
+    ],
+    [
+        'id'=> 3,
+        'name'=>'Basuoi'
+
+    ],
+    [
+        'id'=> 4,
+        'name'=>'Abosalem'
+    ]
+
+
+
+
+
+
+];
+
+
+
+
+return $students;
+
+
+}
+
+
+
+
+
+
+
+}
